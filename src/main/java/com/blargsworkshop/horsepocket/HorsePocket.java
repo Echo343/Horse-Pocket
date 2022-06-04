d8718ba6a2b4c2e5030787c40b22deca3144c790
Echo343 <Echo343@users.noreply.github.com>
2022-06-04 03:08:25 -0700

    Share the same creative tab

--- src/main/java/com/blargsworkshop/horsepocket/HorsePocket.java
+++ src/main/java/com/blargsworkshop/horsepocket/HorsePocket.java
@@ -1,5 +1,8 @@
 package com.blargsworkshop.horsepocket;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import com.blargsworkshop.horsepocket.init.Registry;
 
 import net.minecraft.world.item.CreativeModeTab;
@@ -11,24 +14,43 @@ import net.minecraftforge.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("horsepocket")
-public class HorsePocket
-{
-	public static final boolean DEBUG = false;
-	
-    // Directly reference a log4j logger.
-//    private static final Logger LOGGER = LogManager.getLogger();
-    
-    public static final String MOD_ID = "horsepocket";
+public class HorsePocket {
 
-    public HorsePocket() {
-        Registry.init();
-    }
-    
-    public static final CreativeModeTab TAB = new CreativeModeTab("blargsTab") {
-        @Override
-		@OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(Blocks.SMITHING_TABLE);
-        }
-    };
+	public static final boolean DEBUG = false;
+
+	// Directly reference a log4j logger.
+//    private static final Logger LOGGER = LogManager.getLogger();
+
+	public static final String MOD_ID = "horsepocket";
+	public static final CreativeModeTab TAB;
+
+	static {
+		Optional<CreativeModeTab> op = Arrays.stream(CreativeModeTab.TABS).filter((tab) -> {
+			return tab.getRecipeFolderName().equalsIgnoreCase("blargsTab");
+		}).findFirst();
+
+		if (op.isPresent()) {
+			TAB = op.get();
+		} else {
+			TAB = new CreativeModeTab("blargsTab") {
+				@Override
+				@OnlyIn(Dist.CLIENT)
+				public ItemStack makeIcon() {
+					return new ItemStack(Blocks.SMITHING_TABLE);
+				}
+			};
+		}
+	}
+
+	public HorsePocket() {
+		Registry.init();
+	}
+
+//    public static final CreativeModeTab TAB = new CreativeModeTab("blargsTab") {
+//        @Override
+//		@OnlyIn(Dist.CLIENT)
+//        public ItemStack makeIcon() {
+//            return new ItemStack(Blocks.SMITHING_TABLE);
+//        }
+//    };
 }
