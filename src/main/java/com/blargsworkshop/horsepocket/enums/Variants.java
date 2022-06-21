cf453c898a7403d1b1f48405d578b56c0d3b9b93
Echo343 <Echo343@users.noreply.github.com>
2022-06-21 12:28:38 -0700

    Updated variant text
    This should help with breeding.

--- src/main/java/com/blargsworkshop/horsepocket/enums/Variants.java
+++ src/main/java/com/blargsworkshop/horsepocket/enums/Variants.java
@@ -19,7 +19,7 @@ public enum Variants {
 		vars.put(5, "a_gray_horse");
 		vars.put(6, "a_dark_brown_horse");
 		
-		vars.put(256, "a_white_horse");
+		vars.put(256, "a_white_horse_with_white_accents");
 		vars.put(257, "a_creamy_horse_with_white_accents");
 		vars.put(258, "a_chestnut_horse_with_white_accents");
 		vars.put(259, "a_brown_horse_with_white_accents");
@@ -27,7 +27,7 @@ public enum Variants {
 		vars.put(261, "a_gray_horse_with_white_accents");
 		vars.put(262, "a_dark_brown_horse_with_white_accents");
 		
-		vars.put(512, "a_white_horse");
+		vars.put(512, "a_white_horse_with_white_fields");
 		vars.put(513, "a_creamy_horse_with_white_fields");
 		vars.put(514, "a_chestnut_horse_with_white_fields");
 		vars.put(515, "a_brown_horse_with_white_fields");
@@ -35,7 +35,7 @@ public enum Variants {
 		vars.put(517, "a_gray_horse_with_white_fields");
 		vars.put(518, "a_dark_brown_horse_with_white_fields");
 		
-		vars.put(768, "a_white_horse");
+		vars.put(768, "a_white_horse_with_white_dots");
 		vars.put(769, "a_creamy_horse_with_white_dots");
 		vars.put(770, "a_chestnut_horse_with_white_dots");
 		vars.put(771, "a_brown_horse_with_white_dots");
@@ -47,7 +47,7 @@ public enum Variants {
 		vars.put(1025, "a_creamy_horse_with_black_dots");
 		vars.put(1026, "a_chestnut_horse_with_black_dots");
 		vars.put(1027, "a_brown_horse_with_black_dots");
-		vars.put(1028, "a_black_horse");
+		vars.put(1028, "a_black_horse_with_black_dots");
 		vars.put(1029, "a_gray_horse_with_black_dots");
 		vars.put(1030, "a_dark_brown_horse_with_black_dots");
 	}
